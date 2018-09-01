89c5b30e7ace77944cb8f262035ccc718e56efce
Sasha Hart <sashanicolehart@gmail.com>
2018-09-01 04:16:57 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+function getFirstSelector(selector) {
+  var firstSelector = document.querySelector(selector);
+  return firstSelector;
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+    var rankedList = document.querySelectorAll('.ranked-list li');
+
+    for ( let i = 0; i < rankedList.length; i++ ) {
+        rankedList[i].innerHTML = (parseInt(rankedList[i].innerHTML, 10) + n).toString();
+    }
+}
+
+function deepestChild() {
+    const deepChild = document.querySelector('#grand-node');
+    var deeperChild = deepChild.querySelectorAll('div');
+
+    var theDeepestChild = deeperChild[deeperChild.length - 1];
+
+    return theDeepestChild;
+}
